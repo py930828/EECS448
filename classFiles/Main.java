ddbe61422d32694f42a85979f9e9201c34124ef9
py930828 <cl930828@gmail.com>
2017-03-31 13:07:03 -0500

    function for printing the matrix in output.txt

--- classFiles/Main.java
+++ classFiles/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(System.getProperty("user.dir"));
+		double det;
 		if(args.length < 2)
 		{
 			System.out.println("Error - Expected usage: ./main input.txt output.txt");
@@ -19,18 +20,36 @@ public class Main {
 			File read = new File(args[0]);
 			sc = new Scanner(read);
 			FileWriter out = new FileWriter(args[1]);
-			int check;
+			int size;
 			while(sc.hasNextLine()){
-				check = sc.nextInt();
+				size = sc.nextInt();
 				//check the size of the Matrix
-				if(check < 0)
+				if(size < 0)
 				{
 					break;
 				}
 				// test
-				Matrix m = new Matrix(check);
+				Matrix m = new Matrix(size);
+				for(int i = 0; i < size; i++)
+				{
+					for(int j = 0; j < size; j++)
+					{
+						m.build(sc.nextInt(), i, j);
+					}
+				}
+				out.write("M = "+ "\r\n");
+				for(int i = 0; i < size; i++){
+					for(int j = 0; j < size; j++)
+					{
+						out.write(m.getMat(i,j));
+						out.write(" ");
+					}
+					out.write("\r\n");
+				}
+			
 				
 			}
+			out.close();
 			
 		}catch (Exception e){
 			System.out.println("Could not find input file: %s"+args[0]);
--- classFiles/Matrix.java
+++ classFiles/Matrix.java
@@ -5,10 +5,21 @@ import java.util.Scanner;
 import java.util.Scanner;
 public class Matrix {
 	private int size;
+	double[][] mat;
 	public Matrix(int n){
 		// check size
-		size = n;
-		System.out.println(size);
+		size = n;	
+		mat = new double[size][size];
+		
+	}
+	void build(int num, int row, int column )
+	{
+		mat[row][column] = num;
+	
+	}
+	String getMat(int row, int column)
+	{
+		return Double.toString(mat[row][column]);
 	}
 
 }
