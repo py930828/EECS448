2b31e6030d8afbea76430a8704a8742144757d9b
Long Chen <l115c442@ku.edu>
2017-03-31 10:45:34 -0500

    testing

---
+++ classFiles/Main.java
@@ -0,0 +1,40 @@
+package classFiles;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(System.getProperty("user.dir"));
+		if(args.length < 2)
+		{
+			System.out.println("Error - Expected usage: ./main input.txt output.txt");
+			System.exit(0);
+		}
+		Scanner sc = null;
+		try {
+			
+			File read = new File(args[0]);
+			sc = new Scanner(read);
+			FileWriter out = new FileWriter(args[1]);
+			int check;
+			while(sc.hasNextLine()){
+				check = sc.nextInt();
+				//check the size of the Matrix
+				if(check < 0)
+				{
+					break;
+				}
+				Matrix m = new Matrix(check);
+				
+			}
+			
+		}catch (Exception e){
+			System.out.println("Could not find input file: %s"+args[0]);
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ classFiles/Matrix.java
@@ -0,0 +1,13 @@
+package classFiles;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Scanner;
+public class Matrix {
+	private int size;
+	public Matrix(int n){
+		size = n;
+		System.out.println(size);
+	}
+
+}
